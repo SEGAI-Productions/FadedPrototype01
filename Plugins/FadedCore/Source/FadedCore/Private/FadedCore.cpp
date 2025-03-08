721960f5eea15fbfdf7c0b17533480136524d063
Mike Lee <rockespecialee@gmail.com>
2025-03-07 17:55:54 -0800

    move gameplay tags to FadedCore

--- Plugins/FadedCore/Source/FadedCore/Private/FadedCore.cpp
+++ Plugins/FadedCore/Source/FadedCore/Private/FadedCore.cpp
@@ -1,4 +1,5 @@
 #include "FadedCore.h"
+#include "GameplayTagsManager.h"
 
 #define LOCTEXT_NAMESPACE "FFadedCoreModule"
 
@@ -6,6 +7,16 @@ void FFadedCoreModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	UE_LOG(LogTemp, Log, TEXT("FadedCore module has been loaded"));
+	
+	// Register the gameplay tags
+	UGameplayTagsManager& TagManager = UGameplayTagsManager::Get();
+	
+	// Register the tags
+	TagManager.AddNativeGameplayTag(FName("Faded.Abilities.Movement.Dodge"));
+	TagManager.AddNativeGameplayTag(FName("Faded.Abilities.Movement.Cover"));
+	
+	// Ensure all tags are registered
+	TagManager.DoneAddingNativeTags();
 }
 
 void FFadedCoreModule::ShutdownModule()
